f2ef4499cab82c37aaaadc543fc226768c7b0bde
Krisinc <ksbeanch@gmail.com>
2026-02-06 17:07:36 +0800

    github page try

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/list/",
+  build: {
+    outDir: "docs"
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
